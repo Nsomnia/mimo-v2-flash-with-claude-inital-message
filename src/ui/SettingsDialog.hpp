f2bd9d82f16559d7734290d65f8cdd385cc35c8d
Nsomnia <ssa.nsomnia@gmail.com>
2025-12-31 17:48:44 -0700

    fix: recording pipeline, preset auto-rotation settings, and UI panel persistence
    - Implemented frame capture in VisualizerWindow and connected it to VideoRecorder.
    - Added audio sample feeding to VideoRecorder in MainWindow.
    - Added 'Shuffle Presets' and 'Auto-Rotate Presets' toggles to Visualizer menu and Settings dialog.
    - Consolidated Tools dock and added visibility tracking for all dock panels.
    - Updated TASKS.md and TODO.md with latest fixes.

--- src/ui/SettingsDialog.hpp
+++ src/ui/SettingsDialog.hpp
@@ -4,41 +4,41 @@
 
 #include "util/Types.hpp"
 
+#include <QCheckBox>
+#include <QComboBox>
 #include <QDialog>
-#include <QTabWidget>
+#include <QDoubleSpinBox>
 #include <QLineEdit>
 #include <QSpinBox>
-#include <QDoubleSpinBox>
-#include <QComboBox>
-#include <QCheckBox>
+#include <QTabWidget>
 
 namespace vc {
 
 class SettingsDialog : public QDialog {
     Q_OBJECT
-    
+
 public:
     explicit SettingsDialog(QWidget* parent = nullptr);
-    
+
 public slots:
     void accept() override;
     void reject() override;
-    
+
 private:
     void setupUI();
     void loadSettings();
     void saveSettings();
-    
+
     QTabWidget* tabWidget_{nullptr};
-    
+
     // General
     QCheckBox* debugCheck_{nullptr};
     QComboBox* themeCombo_{nullptr};
-    
+
     // Audio
     QComboBox* audioDeviceCombo_{nullptr};
     QSpinBox* bufferSizeSpin_{nullptr};
-    
+
     // Visualizer
     QLineEdit* presetPathEdit_{nullptr};
     QSpinBox* vizWidthSpin_{nullptr};
@@ -46,8 +46,9 @@ private:
     QSpinBox* vizFpsSpin_{nullptr};
     QDoubleSpinBox* beatSensitivitySpin_{nullptr};
     QSpinBox* presetDurationSpin_{nullptr};
+    QCheckBox* autoRotateCheck_{nullptr};
     QCheckBox* shufflePresetsCheck_{nullptr};
-    
+
     // Recording
     QLineEdit* outputDirEdit_{nullptr};
     QComboBox* containerCombo_{nullptr};
